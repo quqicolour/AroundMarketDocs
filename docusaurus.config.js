0a9bde0b0713686a95fe4781aad61311d0e0980c
whalehat <ouyangmin125@gmail.com>
2025-12-22 08:00:42 +0800

    feat: 🎸 update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://aroundmarketdocs-strg.ipfs.4everland.app/',
+  url: 'https://aroundmarketdocs-strg.ipfs.4everland.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
